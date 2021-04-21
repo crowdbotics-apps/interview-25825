85e3470addb8c5ebe4d65149590704fee3271e43
jonny <jonathanhanley1234@gmail.com>
2021-04-21 20:33:53 +0100

    Added fields to the Plan model. Created Subscription model and added required fields

--- home/api/admin.py
+++ home/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import App
+from .models import App, Plan
 # Register your models here.
 
 admin.site.register(App)
+admin.site.register(Plan)
--- home/api/models.py
+++ home/api/models.py
@@ -29,7 +29,18 @@ class App(models.Model):
 
 
 class Plan(models.Model):
+    name = models.CharField(name="Name", max_length=20, null=True, blank=True)
+    description = models.TextField(name='Description', null=True, blank=True)
+    price = models.IntegerField(default=0, null=True)
+    created_at = models.DateTimeField(name="Created at", auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(name="Updated at", auto_now=True, null=True, blank=True)
 
 
 class Subscription(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, name="User", on_delete=models.CASCADE, null=True, blank=True)
+    plan = models.ForeignKey("Plan", name="Plan", on_delete=models.CASCADE, null=True, blank=True)
+    app = models.ForeignKey("App", name="App", on_delete=models.CASCADE, null=True, blank=True)
+    active = models.BooleanField(name="Active", default=True)
+
+
+
