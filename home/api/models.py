eff6a1a1e77a21dc0ff3656048a11cb283524774
jonny <jonathanhanley1234@gmail.com>
2021-04-22 13:40:32 +0100

    Added test outline

--- home/api/models.py
+++ home/api/models.py
@@ -17,36 +17,35 @@ class App(models.Model):
         ("DJ", "Django"),
         ("RN", "React Native")
     )
-    name = models.CharField(name="Name", max_length=50, null=True, blank=True)
-    description = models.TextField(name='Description', null=True, blank=True)
-    type = models.CharField(name="Type", max_length=50, choices=APP_TYPES, default="Web", null=True, blank=True)
-    framework = models.CharField(name="Framework", max_length=50, choices=APP_FRAMEWORKS, default="DJ", null=True, blank=True)
-    screenshot = models.ImageField(name="Screenshot", upload_to="App_Screenshots", null=True, blank=True)
-    subscription = models.ForeignKey("Subscription", name="Subscription", on_delete=models.CASCADE, null=True, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, name="User", on_delete=models.CASCADE, null=True, blank=True)
-    created_at = models.DateTimeField(name="Created at", auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(name="Updated at", auto_now=True, null=True, blank=True)
+    name = models.CharField(verbose_name="Name", max_length=50, null=True, blank=True)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    type = models.CharField(verbose_name="Type", max_length=50, choices=APP_TYPES, default="Web", null=True, blank=True)
+    framework = models.CharField(verbose_name="Framework", max_length=50, choices=APP_FRAMEWORKS, default="DJ", null=True, blank=True)
+    screenshot = models.ImageField(verbose_name="Screenshot", upload_to="App_Screenshots", null=True, blank=True)
+    subscription = models.ForeignKey("Subscription", verbose_name="Subscription", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="User", on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Plan(models.Model):
-    name = models.CharField(name="Name", max_length=20, null=True, blank=True)
-    description = models.TextField(name='Description', null=True, blank=True)
-    price = models.IntegerField(default=0, null=True)
-    created_at = models.DateTimeField(name="Created at", auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(name="Updated at", auto_now=True, null=True, blank=True)
+    name = models.CharField(verbose_name="Name", max_length=20, null=True, blank=True)
+    description = models.TextField(verbose_name='Description', null=True, blank=True)
+    price = models.IntegerField(verbose_name=0, null=True)
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, name="User", on_delete=models.CASCADE, null=True, blank=True)
-    plan = models.ForeignKey("Plan", name="Plan", on_delete=models.CASCADE, null=True, blank=True)
-    app = models.ForeignKey("App", name="App", on_delete=models.CASCADE, null=True, blank=True)
-    active = models.BooleanField(name="Active", default=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="User", on_delete=models.CASCADE, null=True, blank=True)
+    plan = models.ForeignKey("Plan", verbose_name="Plan", on_delete=models.CASCADE, null=True, blank=True)
+    active = models.BooleanField(verbose_name="Active", default=True)
 
     def __str__(self):
         return "{}-{}".format(self.plan.name, self.active)
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from home.api.models import *
 
-# Create your tests here.
+
+class ApiAppTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        User.objects.create(
+            username="Jonny",
+            email="TestEmail@gmail.com",
+            password="MySecurePassword123"
+        )
+
+    def test_create_app(self):
+        User = get_user_model()
+        user = User.objects.get(email="TestEmail@gmail.com")
+        self.assertEqual(user.email, "TestEmail@gmail.com")
+        name = "Super Cool App"
+        description = "This is my super cool app for testing"
+        framework = "DJ"
+        # subscription
+        # User = get_user_model()
+        # app = App.objects.create(
+        #     Name=name,
+        #     description=description,
+        #
+        # )
